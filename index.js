6990fce9e7c46a23a40989d1bdbce86ba1b07fda
lasjorg <28780271+lasjorg@users.noreply.github.com>
2020-03-29 18:16:20 +0200

    fix: implementation of IntersectionObserver

--- index.js
+++ index.js
@@ -1,73 +1,41 @@
-const menuToggle = document.getElementById('menu-toggle');
-const dropdownMenu = document.getElementById('dropdown-menu');
-const searchGrid = document.getElementById('search-grid-scroll-trigger');
-const searchTop = document.getElementById('top-scroll-trigger');
-const header = document.querySelector('.header');
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menu-toggle');
+  const dropdownMenu = document.getElementById('dropdown-menu');
+  const topSearchST = document.getElementById('search-results-scroll-trigger');
+  const headerST = document.getElementById('header-scroll-trigger');
+  const header = document.querySelector('.header');
 
-document.addEventListener('scroll', () => {
-  window.pageYOffset > 20
-    ? header.classList.add('has-scroll')
-    : header.classList.remove('has-scroll');
-});
-
-menuToggle.addEventListener('click', () => {
-  dropdownMenu.classList.toggle('hide');
-});
-
-// const sections = document.querySelectorAll('div.screen');
-const gridObserverConfig = {
-  // rootMargin: '-50px 0px -55%',
-  threshold: 0.6
-};
-
-const gridObserver = new IntersectionObserver(function(entries, self) {
-  entries.forEach(entry => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      gridIntersectionHandler(entry);
-    } else {
-      header.classList.remove('has-scroll-to-target');
-    }
+  menuToggle.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('hide');
   });
-}, gridObserverConfig);
 
-gridObserver.observe(searchGrid);
+  /* IntersectionObserver for giving the header a gradient black when the page
+   * is scrolled down about 20px
+   */
+  const headerObserver = new IntersectionObserver((entries, self) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        header.classList.add('has-scroll');
+      } else {
+        header.classList.remove('has-scroll');
+      }
+    });
+  });
 
-function gridIntersectionHandler(entry) {
-  const id = entry.target.id;
+  headerObserver.observe(headerST);
 
-  if (id === 'search-grid-scroll-trigger') {
-    console.log('in grid');
-    header.classList.remove('has-scroll');
-    header.classList.add('has-scroll-to-target');
-  }
-}
+  /* IntersectionObserver for making the header solid black when the page
+   * is scrolled down to where the header is on top of the search grid element
+   */
+  const topSearchObserver = new IntersectionObserver((entries, self) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        header.classList.add('has-scroll-to-target');
+      } else {
+        header.classList.remove('has-scroll-to-target');
+      }
+    });
+  });
 
-// const topObserverConfig = {
-//   // rootMargin: '-50px 0px -55%',
-//   threshold: 0.8
-// };
-
-// const topObserver = new IntersectionObserver(function(entries, self) {
-//   entries.forEach(entry => {
-//     console.log(entry);
-//     if (entry.isIntersecting) {
-//       header.classList.add('has-scroll');
-//     } else {
-//       header.classList.remove('has-scroll');
-//     }
-//   });
-// }, topObserverConfig);
-
-// topObserver.observe(searchTop);
-
-// function gridIntersectionHandler(entry) {
-//   const id = entry.target.id;
-//   console.log(id);
-//   console.log(window.pageYOffset);
-
-//   if (id === 'search-top-featured') {
-//     console.log('in search');
-//     header.classList.add('has-scroll');
-//   }
-// }
+  topSearchObserver.observe(topSearchST);
+});
